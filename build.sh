85cecd82a5052f4858022533fd2e45e582240c3d
my token <vamsipabbiti2004@gmail.com>
2026-07-11 16:24:50 +0530

    Fix build using node to run vite directly

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Build frontend
 cd frontend
 npm install
-./node_modules/.bin/vite build
+npm run build
 
-# Copy into backend/static
 rm -rf ../backend/static
 cp -r dist ../backend/static
 
-# Install backend dependencies
 cd ../backend
 pip install -r requirements.txt
