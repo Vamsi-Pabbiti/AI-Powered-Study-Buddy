392c01ce1234b1d36f9519916fa7863918e985c5
my token <vamsipabbiti2004@gmail.com>
2026-07-31 22:41:38 +0530

    Pin bcrypt and passlib versions

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 ﻿#!/usr/bin/env bash
 set -o errexit
 
+# Build frontend
 cd frontend
 npm install
 npm run build
 
+# Copy into backend/static
 rm -rf ../backend/static
 cp -r dist ../backend/static
 
+# Install backend deps
 cd ../backend
 pip install -r requirements.txt
